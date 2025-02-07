04b65277fc8c36ae491389d89fbb351654c6f09e
tigranfah <tigran.fahradyan01@gmail.com>
2025-02-07 12:52:18 +0400

    add the missing parts of the assembly code

--- math.s
+++ math.s
@@ -8,11 +8,11 @@ math_func:
    # this moves variable i from the memory to the %rax register
    mov i, %rax 
    # write something to move variable j to the %rbx register
-   ??????????
+   mov j, %rbx
    # do j - i
-   sub  ?, ? 
+   sub %rax, %rbx
    # at the end of the function %rax should contain the return of the function
    # do something so for that:
-   ??????????
+   mov %rbx, %rax
    #function exits
    ret
